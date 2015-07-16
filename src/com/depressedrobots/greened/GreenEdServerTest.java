4dbf543e6e995f62ffb693983ef102022fbc08b0
Wojciech A. Jurczyk <wajurczyk@gmail.com>
2015-07-16 20:52:26 +0200

    added default root handler and test

--- src/com/depressedrobots/greened/GreenEdServerTest.java
+++ src/com/depressedrobots/greened/GreenEdServerTest.java
@@ -2,6 +2,8 @@ package com.depressedrobots.greened;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -9,37 +11,13 @@ import org.junit.rules.ExpectedException;
 public class GreenEdServerTest {
 	
 	@Rule
-	public ExpectedException exception = ExpectedException.none();
-	
-	@Test
-	public void testMain_noArguments() {
-		exception.expect(IllegalArgumentException.class);
-		exception.expectMessage("No port provided in arguments!");
-		
-		GreenEdServer.main(null);
-	}
-	
-	@Test
-	public void testMain_tooManyArguments() {
-		exception.expect(IllegalArgumentException.class);
-		exception.expectMessage("Too many arguments provided!");
+	public ExpectedException expected = ExpectedException.none();
 
-		GreenEdServer.main(new String[]{"1234", "5678"});
-	}
-	
 	@Test
-	public void testMain_argumentNotAnInteger() {
-		exception.expect(IllegalArgumentException.class);
-		exception.expectMessage("Argument not an Integer!");
-		
-		GreenEdServer.main(new String[]{"ABCD"});
+	public void test_InvalidPort() throws IOException {
+		Integer port = 80;	/* some invalid portnumber */
+		expected.expect(IOException.class);
+		new GEServer(port);
 	}
-	
-	@Test
-	public void testMain_argumentIsANegativeInteger() {
-		exception.expect(IllegalArgumentException.class);
-		exception.expectMessage("Port number is negative!");
-		
-		GreenEdServer.main(new String[]{"-123"});
-	}
+
 }
